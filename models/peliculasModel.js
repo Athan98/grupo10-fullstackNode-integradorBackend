baeab44b522556f2ece00d593a9b3c03c7384478
Leonardo <leonardozacca@hotmail.com>
2024-07-09 03:18:48 -0300

    corrigiendo error de sintaxis

--- models/peliculasModel.js
+++ models/peliculasModel.js
@@ -1,27 +1,30 @@
-const db = require ("../data/db")
-
-const {DataTypes} = require ("sequelize")
+const db = require("../data/db");
+const { DataTypes } = require("sequelize");
 
 const peliculasModel = db.define("peliculas", {
-    titulo:{
-        type:DataTypes.STRING
-        allowNull: false,     //no permite introducción de valores nulos
+  titulo: {
+    type: DataTypes.STRING,
+    allowNull: false, // No permite valores nulos
+  },
+  descripcion: {
+    type: DataTypes.STRING,
+  },
+  genero: {
+    type: DataTypes.STRING,
+  },
+  duracion: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0, // Duración debe ser un número positivo
     },
+  },
+  director: {
+    type: DataTypes.STRING,
+  },
+  fecha_estreno: {
+    type: DataTypes.DATE,
+  },
+});
 
-    descripcion:{type:DataTypes.STRING},
-
-    genero:{type:DataTypes.STRING},
-
-    duracion:{
-        type:DataTypes.INTEGER
-        validate : {
-            min: 0  //la duración debe ser un número positivo
-        }
-    },
-
-    director:{type:DataTypes.STRING},
-
-    fecha_estreno:{type:DataTypes.DATE},
-})
-
-module.exports = peliculasModel
+module.exports = peliculasModel;
